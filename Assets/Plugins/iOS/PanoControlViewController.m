9f5b31d72e5f453191bf1380487da8106961c7bb
薛宇涛 <xueyutao@xueyutaodeMacBook-Air.local>
2018-07-20 12:53:32 +0800

    增加全景图返回uniy按钮

--- Assets/Plugins/iOS/PanoControlViewController.m
+++ Assets/Plugins/iOS/PanoControlViewController.m
@@ -40,12 +40,36 @@ UIView * unityView = nil; //内存指针
     unityView = UnityGetGLView();
     [unityView addSubview:self.panoramaView];
     
+    CGRect cbtnFrame = CGRectMake(0, 100, 50, 30);
+    UIButton *testBtn = [self createButton:@"返回" target:@selector(onTestBtn) frame:cbtnFrame];
+    [self.panoramaView addSubview:testBtn];
+    
     // 设定全景的清晰度， 默认为middle
     [self.panoramaView setPanoramaImageLevel:ImageDefinitionHigh];
     
     // 设定全景的pid， 这是指定显示某地的全景，也可以通过百度坐标进行显示全景
     //[self.panoramaView setPanoramaWithLon:116.40391285827147 lat:39.91403075654526];
     [self.panoramaView setPanoramaWithLon:longitude lat:latitude];
+}
+
+- (UIButton *)createButton:(NSString *)title target:(SEL)selector frame:(CGRect)frame {
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    button.frame = frame;
+    [button setTitle:title forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    if ( [[[UIDevice currentDevice] systemVersion] floatValue] >= 7 ) {
+        [button setBackgroundColor:[UIColor whiteColor]];
+    } else {
+        [button setBackgroundColor:[UIColor clearColor]];
+    }
+    [button addTarget:self action:selector forControlEvents:UIControlEventTouchUpInside];
+    return button;
+}
+
+- (void)onTestBtn {
+    //[self.panoramaView setCustomOverlayAnchor:@"54321" x:0.5f y:0.1f];
+    NSLog(@"this is a button");
+    [self.panoramaView removeFromSuperview];
 }
 
 @end
